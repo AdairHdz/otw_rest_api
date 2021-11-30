4bb9539768b190d9d45f0acf321252ab9a0106a0
AdairHdz <adairho16@gmail.com>
2021-11-30 01:22:32 -0600

    chore: Se corrigió un bug en la funcionalidad de enviar y refrescar código de activación. El servidor retornaba un 401 debido a que no recibía un token de autenticación, ya que en la respuesta de registro no se contemplaba el envío del mismo. Cambios: * Se añadió un JWT token a la respuesta de registro. * Se añadieron los campos UserID y Token a la respuesta de registro. * Se añadió el campo UserID al mapper de la respuesta de registro.

--- response/user_response.go
+++ response/user_response.go
@@ -2,9 +2,11 @@ package response
 
 type User struct {
 	ID 			 string `json:"id"`
+	UserID string `json:"userId"`
 	Names        string `json:"names"`
 	LastName     string `json:"lastName"`
 	EmailAddress string `json:"emailAddress"`
 	UserType     int    `json:"userType"`
 	StateID      string `json:"stateId"`
+	Token string `json:"token"`
 }
